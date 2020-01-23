6cb78022ca5fff51f86260df3066058e4c44ddaf
roronya <roronya628@gmail.com>
2020-01-23 11:16:26 +0900

    add PrefixExpression

--- object/object.go
+++ object/object.go
@@ -32,4 +32,4 @@ func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 type Null struct{}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
-func (n *Null) Inspcet() string  { return "null" }
+func (n *Null) Inspect() string  { return "null" }
